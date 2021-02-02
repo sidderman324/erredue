e68a9ebf69610d462df70d39d5744c1f32e2184f
Sergey Sidorov <s.i.sidorovkrd@yandex.ru>
2021-02-02 20:44:35 +0300

    Восстановлена главная страница

--- wp-content/themes/erredue/_frontend/index.php
+++ wp-content/themes/erredue/_frontend/index.php
@@ -9,5 +9,63 @@
 </head>
 <body>
 
+  <?php include ('modules/header.php');?>
+  <?php include ('modules/svg_icons.php');?>
+
+  <section class="promo" style="background-image: url('/wp-content/themes/erredue/static/imgs/bgr_1.jpg');">
+    <div class="container">
+
+      <div class="text_box">
+        <p class="text text_size_xl white bold">PRODUCI IL TUO GAS DOVE SERVE</p>
+        <h1 class="title text_size_xxxl white bold">CHE GAS VUOI GENERARE?</h1>
+      </div>
+
+      <div class="button_box">
+        <a href="#" class="btn btn--blue text_size_l btn--right-arrow">GAS PER LABORATORIO</a>
+        <a href="#" class="btn btn--white text_size_l btn--right-arrow">GAS INDUSTRIALI</a>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="mission_block">
+    <div class="container">
+      <h2 class="mission_title blue bold">MISSION</h2>
+      <span class="blue_line"></span>
+      <p class="text text_size_l">Sviluppare tecnologie per aiutare le aziende a migliorare la produzione, la sicurezza e la qualità del lavoro.</p>
+    </div>
+  </section>
+
+
+  <section class="image_bgr" style="background-image: url('/wp-content/themes/erredue/static/imgs/bgr_2.jpg');">
+    <div class="container">
+
+      <div class="text_blue_box">
+        <p class="text text_size_l white">
+          ErreDue progetta, realizza e commercializza generatori, miscelatori e purificatori di gas, con portate che vanno da pochi litri/ora a centinaia di metri cubi/ora.
+        </p>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="produced_block">
+    <div class="container">
+      <div class="text_box">
+        <h2 class="section_title text_size_xxl blue">
+          Proudly Designed and made in italy
+          <span class="flag_separator"></span>
+        </h2>
+
+        <p class="text text_size_l">Ogni nostro prodotto nasce con l’idea di fornire soluzioni che rendano autosufficienti le aziende nella produzione di gas tecnici attraverso tecnologie sicure; il nostro obiettivo è eliminare l’uso delle bombole allontanando i rischi derivanti dallo stoccaggio di materiali potenzialmente pericolosi, assicurando continuità e qualità alla produzione e liberando risorse per l’azienda.</p>
+      </div>
+
+      <div class="produced_circle text_size_l">SCOPRI l’AZIENDA</div>
+    </div>
+  </section>
+
+
+  <?php include ('modules/footer.php');?>
+
 </body>
 </html>
---
+++ wp-content/themes/erredue/_frontend/modules/header.php
@@ -0,0 +1,28 @@
+<div class="page-header">
+  <div class="container">
+    <div class="logo_wrapper">
+      <a href="#">
+        <img src="/wp-content/themes/erredue/static/imgs/erredue_logo_header_small.png" alt="">
+      </a>
+    </div>
+
+    <nav class="main_menu">
+      <ul>
+        <li><a href="#" class="main_menu_link">HOME</a></li>
+        <li><a href="#" class="main_menu_link">PROFILO AZIENDALE</a></li>
+        <li><a href="#" class="main_menu_link">PRODOTTI</a></li>
+        <li><a href="#" class="main_menu_link">APPLICAZIONI</a></li>
+        <li><a href="#" class="main_menu_link">INSTALLAZIONI</a></li>
+        <li><a href="#" class="main_menu_link">REFERENZE</a></li>
+        <li><a href="#" class="main_menu_link">CERTIFICAZIONI</a></li>
+        <li><a href="#" class="main_menu_link">CONTATTI</a></li>
+      </ul>
+    </nav>
+
+    <div class="lang_box">
+      <a href="#"><img src="/wp-content/themes/erredue/static/imgs/flag_it.png" alt=""></a>
+      <a href="#"><img src="/wp-content/themes/erredue/static/imgs/flag_eng.png" alt=""></a>
+      <a href="#"><img src="/wp-content/themes/erredue/static/imgs/flag_rus.png" alt=""></a>
+    </div>
+  </div>
+</div>
