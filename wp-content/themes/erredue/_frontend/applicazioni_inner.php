6ab8514e660907be8ed9c2d40a3e12e6323fb1cd
Sergey Sidorov <s.i.sidorovkrd@yandex.ru>
2021-02-04 13:37:21 +0300

    Внутренние страницы

---
+++ wp-content/themes/erredue/_frontend/applicazioni_inner.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="/wp-content/themes/erredue/static/css/style.css">
+</head>
+<body>
+
+  <?php include ('modules/header.php');?>
+  <?php include ('modules/svg_icons.php');?>
+
+  <section class="promo promo--header promo--header-small" style="background-image: url('/wp-content/themes/erredue/static/imgs/catalog/applicazioni_head_img.jpg');">
+    <div class="container">
+
+      <div class="text_box">
+        <a href="#" class="link text_size_l white">APPLICAZIONI</a>
+        <h1 class="title text_size_xxxl white bold"><span>///</span> TAGLIO LASER</h1>
+      </div>
+
+    </div>
+  </section>
+
+
+
+  <section class="textpage">
+    <div class="container">
+      <h2>TAGLIO LASER</h2>
+
+      <p>ErreDue realizza due prodotti specificamente dedicati al taglio laser: NitroBox e Saturn</p>
+
+      <h2>TAGLIO LASER</h2>
+
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+
+      <p class="bold">Vantaggi della produzione di azoto on site:</p>
+
+      <ul>
+        <li>Produzione on site significa non avere tempo di fermo macchina per sostituire le bombole, nessun costo per il trasporto del gas, nessuno spreco di gas dovuto alla bassa pressione residua nelle bombole.</li>
+        <li>Produzione on site significa maggiore sicurezza per l’operatore laser, eliminando la necessità di usare bombole ad alta pressione o azoto liquido.</li>
+        <li>Produzione on site significa qualità del gas costante, senza fluttuazioni dovute a differenti lotti di gas o a contaminazione del gas in bombola.</li>
+      </ul>
+
+      <h2>SATURN</h2>
+
+      <p>NitroBox è un sistema semplice e compatto per la produzione di azoto pressurizzato direttamente dove serve, per taglio laser ed ogni altra applicazione dove c’è bisogno di azoto puro e compresso.</p>
+
+    </div>
+  </section>
+
+
+  <section class="feedback">
+    <div class="container">
+      <div class="form_block">
+        <div class="head">
+          <p class="title text_size_xxl white">RICHIEDI INFORMAZIONI SU GENERATORI DI IDROGENO SIRIO</p>
+        </div>
+
+        <form action="" method="post">
+          <div class="column">
+            <div class="input_box"><input type="text" name="" placeholder="Nome"></div>
+            <div class="input_box"><input type="text" name="" placeholder="Cognome"></div>
+            <div class="input_box"><input type="text" name="" placeholder="Email"></div>
+          </div>
+          <div class="column">
+            <div class="input_box">
+              <textarea name="name" placeholder="Richiesta"></textarea>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="custom_checkbox">
+              <input id="confirmation" type="checkbox">
+              <label for="confirmation" name="confirmation"><span><a href="#">Leggi le Condizioni di Sicurezza e Privacy</a>: consenso al trattamento dei dati personali ai sensi del D.Lgs 196/2003 "Protezione dei dati personali"<br>Questo sito è protetto da reCAPTCHA e si applicano le norme sulla <a href="#">Privacy ed i Termini di servizio</a> di Google.</span></label>
+            </div>
+          </div>
+
+          <input type="submit" value="invia" class="submit_btn">
+        </form>
+      </div>
+    </div>
+  </section>
+
+
+
+  <?php include ('modules/footer.php');?>
+
+</body>
+</html>
---
+++ wp-content/themes/erredue/_frontend/product_card.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="/wp-content/themes/erredue/static/css/style.css">
+</head>
+<body>
+
+  <?php include ('modules/header.php');?>
+  <?php include ('modules/svg_icons.php');?>
+
+
+  <section class="product_card">
+
+    <div class="head">
+      <div class="container">
+
+        <div class="img_wrapper">
+          <img src="/wp-content/themes/erredue/static/imgs/catalog/prod_1_big.jpg" alt="">
+        </div>
+
+        <div class="text_box">
+          <a href="#" class="category_link text_size_xxl blue">PRODOTTI PER L'INDUSTRIA</a>
+          <h1 class="title text_size_xxxl blue">GENERATORI DI IDROGENO SIRIO</h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="content_box">
+      <div class="container">
+        <h2>GENERATORI DI IDROGENO SIRIO</h2>
+
+        <p>Il nuovo generatore di idrogeno on site Sirio, in virtù degli altissimi livelli di purezza garantiti dalla tecnologia PEM (membrana a scambio protonico) ErreDue, risponde perfettamente alle esigenze di tutte quelle applicazioni dove si renda necessaria una moderata quantità di gas puro e compresso, come ad esempio il trattamento termico dei metalli e i processi di produzione di Biometano.</p>
+        <p>Sirio è Plug & Play: grazie alla sua semplicità di utilizzo e alla sua autonomia è sufficiente collegarlo ad una fonte di alimentazione elettrica e riempire di acqua il serbatoio per poter produrre da subito idrogeno purissimo alla pressio- ne e alla portata necessarie. Non necessita, infatti, di sistemi ausiliari: nell’unità sono compresi generatore, serbatoio acqua e sistema di purificazione del gas prodotto.</p>
+        <p>La portata di idrogeno parte dagli 0,5 m3/h fino ad arrivare ai 2 m3/h, è possibile anche collegare più sistemi in paral- lelo. La pressione massima generata arriva fino ai 15 bar nei modelli a catalogo ed è personalizzabile fino a 30 bar su richiesta.</p>
+        <p>I generatori di idrogeno Sirio ErreDue possono essere monitorati e gestiti a distanza grazie alla connettività integrata e al servizio di assistenza remota ErreDue.</p>
+
+        <a href="#">Scarica la brochure Generatori di idrogeno Sirio</a>
+
+        <div class="gallery">
+          <div class="gallery_item"><img src="/wp-content/themes/erredue/static/imgs/catalog/prod_1_gallery_1.jpg" alt=""></div>
+          <div class="gallery_item"><img src="/wp-content/themes/erredue/static/imgs/catalog/prod_1_gallery_2.jpg" alt=""></div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+
+  <?php include ('modules/footer.php');?>
+
+</body>
+</html>
